d96af5a971fa3fae6dc832b98bf1d80be54f7f3c
NewCastile <carlosaguanipa1@gmail.com>
2024-06-16 23:54:23 -0400

    update for media query based styling

--- src/components/examples/templates/holy-grail/scripts/all-sticky/apply-sticky-navs-top.ts
+++ src/components/examples/templates/holy-grail/scripts/all-sticky/apply-sticky-navs-top.ts
@@ -1,17 +1,49 @@
-const $allStickyHolyGrailHeader = document.querySelector<HTMLElement>(
-	"[data-all-sticky-holy-grail] header"
-)
-const $allStickyHolyGrailNavContainer = document.querySelector<HTMLElement>(
-	"[data-all-sticky-holy-grail] #main-navigation-container"
-)
-const $allStickyHolyGrailAside = document.querySelector<HTMLElement>(
-	"[data-all-sticky-holy-grail] aside nav"
-)
+import tailwindTheme from "../../../../../../../tailwind.config.mjs"
 
-if ($allStickyHolyGrailNavContainer && $allStickyHolyGrailAside && $allStickyHolyGrailHeader) {
-	$allStickyHolyGrailAside.setAttribute("style", `top: ${$allStickyHolyGrailHeader.offsetHeight}px`)
-	$allStickyHolyGrailNavContainer.setAttribute(
-		"style",
-		`top: ${$allStickyHolyGrailHeader.offsetHeight}px`
+const {
+	theme: {
+		extend: { screens },
+	},
+} = tailwindTheme
+
+const mq = window.matchMedia(`(max-width: ${screens.sm})`)
+
+const applyStickyNavsTop = (md: MediaQueryList) => {
+	const $heading = document.querySelector<HTMLElement>("[data-all-sticky-holy-grail] header h2")
+	const $button = document.querySelector<HTMLElement>(
+		"[data-all-sticky-holy-grail] #toggle-nav-button"
 	)
+	const $buttonContainer = $button?.parentElement
+
+	const $stickyContainers = Array.from(
+		document.querySelectorAll("[data-all-sticky-holy-grail] [data-sticky-container]")
+	)
+
+	const $mobileStickyContainer = document.querySelector<HTMLElement>(
+		"[data-all-sticky-holy-grail] [data-mobile-sticky-container]"
+	)
+
+	if ($heading && $button && $mobileStickyContainer && $buttonContainer) {
+		if (md.matches) {
+			$mobileStickyContainer.setAttribute(
+				"style",
+				`top: calc(${$heading.offsetHeight}px + ${$buttonContainer.offsetHeight}px)`
+			)
+		} else {
+			$stickyContainers.forEach((stickyContainer) => {
+				stickyContainer.setAttribute(
+					"style",
+					`
+						top: ${$heading.offsetHeight}px;
+					`
+				)
+			})
+		}
+	}
 }
+
+applyStickyNavsTop(mq)
+
+mq.addEventListener("change", () => {
+	applyStickyNavsTop(mq)
+})
