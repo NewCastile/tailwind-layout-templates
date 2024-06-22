c0f511e5287c2a12074769c7cb8c38eec05efe48
NewCastile <carlosaguanipa1@gmail.com>
2024-06-22 13:24:38 -0400

    update for theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from "tailwindcss/defaultTheme"
-
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			screens: {
 				...defaultTheme.screens,
+			},
+			colors: {
+				...defaultTheme.colors,
+				primary: "#18191a",
+				foreground: "#292929",
+				secondary: "#beb8b0",
 			},
 		},
 	},
